468b122b207aad48fa47b36a149ee3a6afa794d4
Leo Laporte <leo@laporte.email>
2023-12-09 09:02:11 -0800

    2023 Day 1 done

--- 2022/day16/day16.lisp
+++ 2022/day16/day16.lisp
@@ -6,7 +6,7 @@
 ;; Prologue code for setup - same every day
 ;; -----------------------------------------------------------------------------
 
-(ql:quickload '(:fiveam :cl-ppcre :str))
+(ql:quickload '(:fiveam :cl-ppcre))
 
 (defpackage :day16
   (:use #:cl)
@@ -22,7 +22,7 @@
 
 (defparameter *data-file* "~/cl/AOC/2022/day16/input.txt")  ;  data from AoC
 
-#| --------------------- Day 16: Proboscidea Volcanium -------------------------
+#| --------------------- Day 16: Proboscidea Volcanium ----------------------
 
 You scan the cave for other options and discover a network of pipes and
 pressure-release valves. You aren't sure how such a system got into a volcano,
@@ -43,18 +43,19 @@ problem. But we can only make 30 moves to get the most pressure so it's a
 highest pressure problem, not shortest path. It's kind of a hybrid.
 
 Additionally, the problem input has 66 valves, but only around 16 are
-unbroken. So I think that means we can think of the 0 flow rooms as just part of
-a longer path.
+unbroken. So I think that means we can think of the 0 flow rooms as just
+part of a longer path.
 
-I think I can use Dijkstra for this but I need to build a table of valves and
-the shortest travel time between them ignoring any valves that don't
+I think I can use Dijkstra for this but I need to build a table of valves
+and the shortest travel time between them ignoring any valves that don't
 work (flow=0) e.g. (DD . II) => 2. I'll do this using the Floyd-Warshall
 algorithm.
 
-The set of working valves is small enough that I probably could brute force the
-optimal path with a depth first search but why not use Dijkstra to make it
-faster? This has been fun because to use Dijkstra I have to modify it to track
-time, additionally I'm looking for a maximum flow, not a minimum distance.
+The set of working valves is small enough that I probably could brute force
+the optimal path with a depth first search but why not use Dijkstra to make
+it faster? This has been fun because to use Dijkstra I have to modify it to
+track time, additionally I'm looking for a maximum flow, not a minimum
+distance.
 
 Fundamentally Dijkstra works by looking at all the neighbors of the starting
 point. It calculates the distance to each then puts them in a priority-queue,
@@ -71,7 +72,11 @@ maximum, I also have to track the time. I'm going to store the best flow
 achieved by a valve and the time left after turning on the valve in the
 structure itself. Let's see how it works.
 
-It's just a tad low. And I notice that it's not choosing the same optimal path that the example is. Hmm.
+It's just a tad low. And I notice that it's not choosing the same optimal path
+that the example is. Hmm. I'm thinking that the issue is the time factor - I
+don't know that a route isn't optimal until I run out of time. In other words,
+Dijkstra's no better than testing all the paths. So it's a depth first search
+for me. I think there are few enough nodes that this will be doable.
 
 ----------------------------------------------------------------------------- |#
 
@@ -90,15 +95,11 @@ It's just a tad low. And I notice that it's not choosing the same optimal path t
 
 (defstruct valve
   ;; basic unchanging valve data provided by the problem
-  (name)                       ; name of the valve (double caps)
-  (rate 0 :type fixnum)        ; rate of flow
-  (tunnels '() :type list)     ; list of tunnels leading away
+  (name "" :type string)             ; name of the valve (double caps)
+  (rate 0 :type fixnum)              ; rate of flow
+  (tunnels '() :type list))     ; list of tunnels leading away
 
-  ;; the following slots are used in the Dijkstra walk
-  (time 0 :type fixnum)        ; time left after turning on valve
-  (total-flow 0 :type fixnum)) ; total flow after adding this valve
-
-(defparameter *infinity* 99999) ; for Floyd-Warshall
+(defconstant +infinity+ 99999 "an arbitrarily large value") ; for Floyd-Warshall
 
 (defparameter *valve-regex*
   (re:create-scanner
@@ -109,19 +110,19 @@ problem set")
 (defun pht (hash)
   "little utility for printing the contents of a hash"
   (loop for k being the hash-keys in hash using (hash-value v)
-	do (format t "~A => ~A~&" k v)))
+        do (format t "~A => ~A~&" k v)))
 
 (defun parse-valves (los)
-  "turn a list of strings describing valves in a cave into a list of valve structures"
+  "converts a list of strings describing valves in a cave into a list of valve structures"
   (labels ((parse-valve (s)
-	     (re:register-groups-bind
-		 (name (#'parse-integer rate) tunnels)
-		 (*valve-regex* s)
+             (re:register-groups-bind
+                 (name (#'parse-integer rate) tunnels)
+                 (*valve-regex* s)
 
-	       (make-valve
-		:name name
-		:rate rate
-		:tunnels (re:split ", " tunnels)))))
+               (make-valve
+                :name name
+                :rate rate
+                :tunnels (re:split ", " tunnels)))))
 
     (mapcar #'parse-valve los)))
 
@@ -138,56 +139,55 @@ doesn't exist"
   nil)
 
 (5a:test get-valve-test
-  (5a:is (equalp (get-valve "AA" *example*)
-		 (make-valve :name "AA" :tunnels '("DD" "II" "BB"))))
-  (5a:is (equalp (get-valve "ZZ" *example*) nil)))
-
+         (5a:is (equalp (get-valve "AA" *example*)
+                        (make-valve :name "AA" :tunnels '("DD" "II" "BB"))))
+         (5a:is (equalp (get-valve "ZZ" *example*) nil)))
 
 ;; calculate the minimum travel time between all nodes using Floyd-Warshall algo
 ;; (it's slow: O(n^3) but runs on this dataset in 0.03 seconds) - only save
 ;; distances to valves that work
 (defun build-dist-table (valves)
-  "given a list of valve structures, return a hash with the keys being a
-cons of any two valves and the value being the shortest time it takes
-to travel between those points"
+  "given a list of valve structures, return a hash with the keys being a cons of
+any two valves and the value being the shortest time it takes to travel between
+those points - ignore non-working valves"
   (let* ((num-valves (length valves))
-	 (dists (make-hash-table :test 'equal
-				 :size (* num-valves num-valves))))
+         (dists (make-hash-table :test 'equal
+                                 :size (* num-valves num-valves))))
 
     ;; set up hash with to and from valves and "infinite" distances
     (dolist (v1 valves)
       (dolist (v2 valves)
-	(setf
-	 (gethash (cons (valve-name v1) (valve-name v2)) dists)
-	 *infinity*)))
+        (setf
+         (gethash (cons (valve-name v1) (valve-name v2)) dists)
+         +infinity+)))
 
     ;; populate all the one move distances
     (dolist (v1 valves)
       (dolist (v2 (valve-tunnels v1))
-	(setf (gethash (cons (valve-name v1) v2) dists) 1)))
+        (setf (gethash (cons (valve-name v1) v2) dists) 1)))
 
     ;; do the floyd-warshall walk through the hash to find the shortest
     ;; distances
     (dolist (i valves)
       (dolist (j valves)
-	(dolist (k valves)
-	  (let ((new-dist (+ (gethash (cons (valve-name i) (valve-name k))
-				      dists)
-			     (gethash (cons (valve-name k) (valve-name j))
-				      dists))))
-	    (when (< new-dist
-		     (gethash (cons (valve-name i) (valve-name j)) dists))
-	      (setf (gethash (cons (valve-name i) (valve-name j)) dists)
-		    new-dist))))))
+        (dolist (k valves)
+          (let ((new-dist (+ (gethash (cons (valve-name i) (valve-name k))
+                                      dists)
+                             (gethash (cons (valve-name k) (valve-name j))
+                                      dists))))
+            (when (< new-dist
+                     (gethash (cons (valve-name i) (valve-name j)) dists))
+              (setf (gethash (cons (valve-name i) (valve-name j)) dists)
+                    new-dist))))))
 
     ;; remove destinations we don't care about
     (loop for vs being the hash-keys in dists using (hash-value dist)
-	  do (when (or (equalp (car vs) (cdr vs))   ; to = from
-		       (zerop                       ; dest valve broken
-			(valve-rate (get-valve (cdr vs) valves))))
-	       (remhash vs dists)))                 ; remove this entry
+          do (when (or (equalp (car vs) (cdr vs))   ; to = from
+                       (zerop                       ; dest valve broken
+                        (valve-rate (get-valve (cdr vs) valves))))
+               (remhash vs dists)))                 ; remove this entry
 
-    dists))
+    dists)) ; return the final hash
 
 (defparameter *example-dists* (build-dist-table *example*))
 
@@ -201,73 +201,68 @@ to travel between those points"
   "return list of valves that neighbor valve"
   (let ((dests '()))
     (loop for from-to being the hash-keys in dists
-	  do (when (equalp (car from-to) valve)
-	       (push (cdr from-to) dests)))
+          do (when (equalp (car from-to) valve)
+               (push (cdr from-to) dests)))
     dests))
 
 (5a:test get-neighbors-test
   (5a:is (equalp (get-neighbors "CC" *example-dists*)
-		 (list "JJ" "HH" "EE" "DD" "BB"))))
+                 (list "JJ" "HH" "EE" "DD" "BB"))))
 
+;; Prep done; it's depth first search time. The termination condition is 30
+;; moves. (There are others but let's go full brute first).  So I have a map of
+;; the cave, all the valves, and the inter-valve distances I should be able to
+;; recurse and try all the 30-move variations, saving the largest so far.
+;; For each recursion I need to track the total flow, total time so far,
+;; Now the DFS using mutually recursive functions open-valve and next-valves
+(defun next-valves (current neighbors time-left total-flow unopened valves distances)
+  "returns a list of total flows for every possible path in graph - recurses with
+open-valve, returns nil when out of time or no more valves to turn on"
+  (cond ((null unopened) nil)           ; all valves open
+        ((null neighbors) nil)          ; dead end
+        ((< time-left 0) nil)           ; out of time
 
-(defun get-best-flow (valve-strings start time)
-  "Perform a Dijkstra search of valves starting at start returning the maximal
-flow we can achieve in time"
-  (do*
-   ((valve-list (parse-valves valve-strings))          ; list of valves
-    (dists (build-dist-table valve-list))              ; inter-valve travel times
-    (q (loop for v in valve-list collect v))           ; priority queue
-    (next (get-valve start valve-list))                ; next node in graph
+        (t (cons (open-valve current (first neighbors) time-left total-flow unopened valves distances)
+                 (next-valves current (rest neighbors) time-left total-flow unopened valves distances)))))
 
-    ;; these update every loop (var initial next)
-    (flow 0 (valve-total-flow next))                   ; best flow so far
-    (time-left time (valve-time next))                 ; time left
-    (path (list start) (cons (valve-name next) path))  ; track path taken
-    (visited (list start)
-	     (cons (valve-name next) visited)))        ; nodes already visited
+(defun open-valve (prev next time-left total-flow unopened valves distances)
+  "opens the next valve and updates time-left, total-flow, and unopened - mututal
+recursion with next-valves"
+  (let* ((time-after-visit (- time-left (gethash (cons prev next) distances)))
+         (time-after-valve-on (- time-after-visit 1))) ; takes 1 minute to turn on valve
 
-   ;; all done?
-   ((<= time-left 0) (values "Out of time" (reverse path) flow))
+    (cond ((< time-after-valve-on 0) total-flow) ; out of time so return total flow
 
-    ;; body of do loop
-    (format t "Visited: ~a Time-left: ~a Flow: ~a~&" visited time-left flow)
-    (dolist (nbr-name (get-neighbors (valve-name next) dists)) ; adjacent valves
+          ((not (member next unopened))   ; already opened
+           (next-valves                   ; so go to neighboring valves
+            next (get-neighbors next distances) time-after-visit total-flow unopened valves distances))
 
-      ;; does this new route increases the total flow?
-      (let* ((nbr-valve (get-valve nbr-name valve-list)) ; get full valve struct
-	     (new-time (- time-left ; time left after travel and turn on
-			  (gethash
-			   (cons (valve-name next) nbr-name)
-			   dists)   ; - travel time
-			  1))       ; - 1 to turn on the valve
+          (t (next-valves  ; otherwise open the valve and update time and flow
+              next
+              (get-neighbors next distances)
+              time-after-valve-on
+              (+ total-flow (* (valve-rate (get-valve next valves)) time-after-valve-on))
+              (remove next unopened)
+              valves
+              distances)))))
 
-	     (new-flow (+ flow      ; how much flow does it add
-			  (* new-time (valve-rate nbr-valve)))))
+(defun get-flows (los start time)
+  "perform a depth-first-search on the valve graph, return the highest flow"
+  (let* ((valves (parse-valves los))
+         (distances (build-dist-table valves))   ; discards non-working valves
+         (unopened (loop for v in valves         ; list of names of unopened valves
+                         when (> (valve-rate v) 0)
+                           collect (valve-name v))))
 
-	;; is this is an improvement over the previous visit?
-	(when (> new-flow (valve-total-flow nbr-valve))
-	  (setf (valve-total-flow nbr-valve) new-flow) ; new best total flow
-	  (setf (valve-time nbr-valve) new-time)   ; time it took to get here
-	  (push nbr-valve q))))                    ; save this valve to queue
-
-    ;; sort the queue by total flow
-    (setf q (sort q
-		  #'(lambda (x y)
-		      (> (valve-total-flow x)
-			 (valve-total-flow y)))))
-
-    (loop ; returns next unvisited valve with highest total flow
-	  (setf next (pop q))
-	  (when (not (member (valve-name next) visited))
-	    (return)))))
-
-
+    (next-valves start (get-neighbors start distances) time 0 unopened valves distances)))
 
 (defun day16-1 (los)
-  (get-best-flow los "AA" 30))
+  (apply #'max (get-flows los "AA" 30)))
 
 (5a:test day16-1-test
   (5a:is (= 1651 (day16-1 *example-data*))))
+
+;;  "AA" "DD" "BB" "JJ" "HH" "EE" "CC"
 
 #| ---------------------------------- Part Two ---------------------------------
 
@@ -276,10 +271,10 @@ flow we can achieve in time"
 
 ;; now solve the puzzle!
 ;;(time (format t "The answer to AOC 2022 Day 16 Part 1 is ~a"
-;;	      (day16-1 (uiop:read-file-lines *data-file*))))
+;;            (day16-1 (uiop:read-file-lines +data-file+))))
 
 ;; (time (format t "The answer to AOC 2022 Day 16 Part 2 is ~a"
-;;	      (day16-2 (uiop:read-file-lines *data-file*))))
+;;            (day16-2 (uiop:read-file-lines +data-file+))))
 
 ;; -----------------------------------------------------------------------------
 ;; Timings with SBCL on M2 MacBook Air with 24GB RAM
---
+++ 2023/Day_25/Day_25.lisp
@@ -0,0 +1,47 @@
+;;;; Day###.lisp
+;;;; 2023 AOC Day ### solution
+;;;; Leo Laporte
+
+;; -----------------------------------------------------------------------------
+;; Prologue code for setup - same every day
+;; -----------------------------------------------------------------------------
+
+(ql:quickload '(:fiveam :cl-ppcre))
+
+(defpackage :day###
+  (:use #:cl)
+  (:local-nicknames
+   (:re :cl-ppcre)
+   (:5a :fiveam)))
+
+(in-package :day###)
+
+(setf 5a:*run-test-when-defined* t)  ; test as we go
+(declaim (optimize (debug 3)))       ; max debugging info
+;; (declaim (optimize (speed 3))     ; max speed if needed
+
+(defconstant +data-file+ "~/cl/AOC/2023/day###/input.txt"
+  "Downloaded from the AoC problem set")
+
+#| -----------------------------------------------------------------------------
+--- Part One ---
+
+----------------------------------------------------------------------------- |#
+
+
+
+#| -----------------------------------------------------------------------------
+--- Part Two ---
+
+------------------------------------------------------------------------------|#
+
+;; now solve the puzzle!
+;; (time (format t "The answer to AOC 2023 Day ### Part 1 is ~a"
+;;	      (day###-1 (uiop:read-file-lines +data-file+))))
+
+;; (time (format t "The answer to AOC 2023 Day ### Part 2 is ~a"
+;;	      (day###-2 (uiop:read-file-lines +data-file+))))
+
+;; -----------------------------------------------------------------------------
+;; Timings with SBCL on M3-Max MacBook Pro with 64GB RAM
+;; -----------------------------------------------------------------------------
