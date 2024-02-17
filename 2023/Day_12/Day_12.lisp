7cfb4110897635fc596e35afee207e342ec69597
Leo Laporte <leo@laporte.email>
2024-02-16 16:05:09 -0800

    2023 Day 12 done - but kinda slow due to my handrolled memoization.

--- 2023/Day_12/Day_12.lisp
+++ 2023/Day_12/Day_12.lisp
@@ -1,23 +1,24 @@
 ;;;; Day12.lisp
+
 ;;;; 2023 AOC Day 12 solution
 ;;;; Leo Laporte
-;;;; 25- January 2023
+;;;; 25 January - 16 February 2023
 
 ;; ----------------------------------------------------------------------------
 ;; Prologue code for setup - same every day
 ;; ----------------------------------------------------------------------------
 
-(ql:quickload '(:fiveam :cl-ppcre :trivia))
+(ql:quickload '(:fiveam :cl-ppcre :trivia :alexandria))
 
 (defpackage :day12
   (:use #:cl #:iterate)  ; use iter instead of LOOP
   (:local-nicknames
-   (:re :cl-ppcre)   ; regular expressions
-   (:tr :trivia)     ; pattern matching
-   (:5a :fiveam)))   ; testing
+   (:re :cl-ppcre)                      ; regular expressions
+   (:tr :trivia)                        ; pattern matching
+   (:al :alexandria)                    ; common utilities
+   (:5a :fiveam)))                      ; testing
 
 (in-package :day12)
-
 (setf 5a:*run-test-when-defined* t)  ; test as we go
 (declaim (optimize (debug 3)))       ; max debugging info
 ;; (declaim (optimize (speed 3))     ; max speed if needed
@@ -48,16 +49,28 @@ and broken springs that meet the given criteria. What is the sum of
 those counts?
 
 LEO'S NOTES: The structure of the program is easy, it's writing the
-code to count the possible arrangements that's tough. It seems like a
-job for regular expressions, actually.
+code to count the possible arrangements that's tough.
 
-1. Separate the digits
+Let's start with the easy part: parsing the input. Each line needs to
+be separated into two segments I'll call springs and groupings. I'll put
+a meaningless . char at the end of each condition list to make the
+check for the final group easier. (Otherwise I get an error comparing
+# to nil.)
 
-2. Remove # blocks that match digits and the corresponding digits
+Thanks to Peter Norvig (whose solution I pretty much copied) and
+Xavdid (who helped me understand what is actually happening) I have a
+recursive solution to part one.
 
-3. Determine the number of ways each ? block can match the remaining
-digits.
+To speed things up for part two - which grows exponentially - I'll use
+memoization. There's a library for that but I wanted to write my
+own. My cache is very simple. I just keep a hash table with the keys
+being the function's arguments and the values the associated
+results. If I've already solved for that argument, just return the
+hash value, otherwise solve it and save the result to the hash.
 
+I'm told this is "top down dynamic programming." Well la-di-da.
+
+Well it's a little disappointing - it took 78 seconds to do part 2. Maybe I should use a memoize library instead of my roll-your-own hash table.
 ---------------------------------------------------------------------------- |#
 
 (defparameter *test-data*
@@ -68,46 +81,174 @@ digits.
     "????.######..#####. 1,6,5"
     "?###???????? 3,2,1"))
 
-(defun count-possibles (str)
-  "given a string reporting the condition of s series of springs, return
-the number of all possible arrangements of operational and broken
-springs"
-  0)
+(defun parse-input (los)
+  "given a list of strings describing a set of springs and their
+groupings, return a tuple containing a spring string and a list of
+integers representing valid groupings of damaged springs"
+  (let ((spring-list '()))
+
+    (dolist (l los)                     ; go line by line
+      (let ((splits (re:split " " l)))  ; split line in two: springs and groupings
+
+        (push (list
+               (concatenate 'list (first splits) '(#\.)) ;
+               (mapcar #'parse-integer ; make groupings a list of ints
+                       (re:all-matches-as-strings "\\d+"
+                                                  (second splits))))
+              spring-list)))            ; push each tuple into a list
+
+    (reverse spring-list)))             ; and return it (after reversing)
+
+(5a:test parse-input-t
+  (5a:is (equal (first (parse-input *test-data*))
+                (list '(#\? #\? #\? #\. #\# #\# #\# #\.) '(1 1 3)))))
+
+;; use memoization to speed up this process (especially in part 2) by
+;; keeping a hash of all the solved spring sub-sequences and their
+;; results - each call to memoize checks to see if I've seen this
+;; argument before and only performs the calculation if it's new
+(defun memoize (fn)
+  (let ((cache (make-hash-table :test 'equal))) ; push each sub-result into hash
+    #'(lambda (&rest args)
+        (or (gethash args cache) ; have I already solved this? yes!
+            (setf (gethash args cache) (apply fn args)))))) ; nope, solve and cache
+
+(defun max-damage (springs)
+  "returns the largest number of possible damaged springs in a list of
+ springs"
+  (+ (count #\# springs) (count #\? springs)))
+
+(5a:test max-damage-test
+  (5a:is (= 6 (max-damage '(#\# #\? #\. #\? #\. #\# #\# #\#)))))
+
+;; Use recursion to split the parsing into small chunks, memoizing the
+;; results
+(defparameter *cc* ; cached count
+  (memoize ; cache each substring result
+   (lambda (tuple)
+     (let ((springs (first tuple))  ; the list of spring chars
+           (groups (second tuple)))  ; the groupings as a list of int
+
+       (cond ((null groups)                 ; we've done all the groups
+              (if (find #\# springs)    ; any leftover #?
+                  0                        ; then fail
+                  1)) ; satisfied all the groups with no leftovers
+
+             ;; insufficient springs remain to do the job
+             ((< (max-damage springs) (apply #'+ groups)) 0)
+
+             (t         ; there are still groups and springs left to check
+              (+
+               ;; does the first chunk of springs satisfy the groups?
+               (if (or
+                    ;; the first group of springs cannot be #
+                    (not (= (max-damage (subseq springs 0 (first groups)))
+                            (first groups)))
+                    ;; the group is followed by #\# (breaking it)
+                    (char= #\# (first (subseq springs (first groups)))))
+                   0   ; then it's a fail
+
+                   ;; else we've got the first group so chop
+                   ;; off that group (and following spring
+                   ;; which must be .) and continue
+                   (funcall *cc* (list (subseq springs (1+ (first groups)))
+                                       (rest groups))))
+
+               ;; if the first spring is operational, drop it and continue
+               (if (char= #\# (first springs))
+                   0              ; damaged and didn't work above so fail
+                   (funcall *cc* (list (rest springs) groups))))))))))
+
+(5a:test funcall-*cc*-test
+  (let ((d (parse-input *test-data*)))
+    (5a:is (= (funcall *cc* (first d)) 1))
+    (5a:is (= (funcall *cc* (second d)) 4))
+    (5a:is (= (funcall *cc* (third d)) 1))
+    (5a:is (= (funcall *cc* (fourth d)) 1))
+    (5a:is (= (funcall *cc* (fifth d)) 4))
+    (5a:is (= (funcall *cc* (sixth d)) 10))))
 
 
-(5a:test count-possibles-test
-  (5a:is (= (count-possibles (nth 0 *test-data*)) 1))
-  (5a:is (= (count-possibles (nth 1 *test-data*)) 4))
-  (5a:is (= (count-possibles (nth 2 *test-data*)) 1))
-  (5a:is (= (count-possibles (nth 3 *test-data*)) 1))
-  (5a:is (= (count-possibles (nth 4 *test-data*)) 4))
-  (5a:is (= (count-possibles (nth 5 *test-data*)) 10)))
-
-
-(defun Day12-1 (los)
+(Defun Day12 (los)
   "given a list of strings reflecting each sprint condition return the
 sum of possible arrangements of operational and broken springs"
   (cond ((null los) 0)
-        (t (+ (count-possibles (first los))
+        (t (+ (funcall *cc* (first los))
               (Day12-1 (rest los))))))
 
-
-(5a:test Day12-1-test
-  (5a:is (Day12-1 *test-data*) 21))
-
+(5a:test Day12-test
+  (5a:is (= (Day12 (parse-input *test-data*)) 21)))
 
 #| ----------------------------------------------------------------------------
 --- Part Two ---
 
+Let's get exponential!!
+
+"unfold the records, on each row, replace the list of spring springs with five copies of itself (separated by ?) and replace the list of contiguous groups of damaged springs with five copies of itself (separated by ,)."
+
+I was prepared for this. I'll unfold the original parsed lines and
+then let's see if my simple memoization is up to the task.
+
+Harumph - at least it completes but the time (78 seconds) is disappointing.
 ---------------------------------------------------------------------------- |#
 
-;; now solve the puzzle!
-;; (time (format t "The answer to AOC 2023 Day 12 Part 1 is ~a"
-;;               (day12-1 (uiop:read-file-lines *data-file*))))
+(defun unfold-input (los)
+  "given a list of lists containing spring springs and groupings return
+ an unfolded list with each condition string repeated five time with a
+ ? in between each, and return a list of groupings repeating the
+ original group five times."
+  (let ((unfolded '()))
 
-;; (time (format t "The answer to AOC 2023 Day 12 Part 2 is ~a"
-;;	      (day12-2 (uiop:read-file-lines *data-file*))))
+    (dolist (l los)
+      (let* ((splits (re:split " " l))
+             (springs (coerce (first splits) 'list))
+             (rules (mapcar #'parse-integer
+                            (re:all-matches-as-strings "\\d+"
+                                                       (second splits)))))
+
+        (push
+         (list
+          (append (rest (iter (repeat 5) (appending (cons #\? springs)))) '(#\.))
+          (iter (repeat 5) (appending rules)))
+         unfolded)))
+
+    (reverse unfolded)))
+
+(5a:test funcall-*cc*-test-2
+  (let ((d (unfold-input *test-data*)))
+    (5a:is (= (funcall *cc* (first d)) 1))
+    (5a:is (= (funcall *cc* (second d)) 16384))
+    (5a:is (= (funcall *cc* (third d)) 1))
+    (5a:is (= (funcall *cc* (fourth d)) 16))
+    (5a:is (= (funcall *cc* (fifth d)) 2500))
+    (5a:is (= (funcall *cc* (sixth d)) 506250))))
+
+(5a:test Day12-2-test
+  (5a:is (= (Day12 (unfold-input *test-data*)) 525152)))
+
+;; now solve the puzzle!
+(time (format t "The answer to AOC 2023 Day 12 Part 1 is ~a"
+              (day12 (parse-input (uiop:read-file-lines *data-file*)))))
+
+(time (format t "The answer to AOC 2023 Day 12 Part 2 is ~a"
+              (day12 (unfold-input (uiop:read-file-lines *data-file*)))))
 
 ;; ----------------------------------------------------------------------------
 ;; Timings with SBCL on M3-Max MacBook Pro with 64GB RAM
 ;; ----------------------------------------------------------------------------
+
+;; The answer to AOC 2023 Day 12 Part 1 is 6488
+;; Evaluation took:
+;; 0.214 seconds of real time
+;; 0.214490 seconds of total run time (0.214301 user, 0.000189 system)
+;; 100.00% CPU
+;; 5,364,672 bytes consed
+
+;; The answer to AOC 2023 Day 12 Part 2 is 815364548481
+;; Evaluation took:
+;; 78.090 seconds of real time
+;; 77.934677 seconds of total run time (77.876157 user, 0.058520 system)
+;; [ Real times consist of 0.045 seconds GC time, and 78.045 seconds non-GC time. ]
+;; [ Run times consist of 0.045 seconds GC time, and 77.890 seconds non-GC time. ]
+;; 99.80% CPU
+;; 190,906,736 bytes consed
