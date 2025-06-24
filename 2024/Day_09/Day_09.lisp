09c067adeeb913d1164dc2c7dfd56d3c5c8e8b53
Leo Laporte <leo@laporte.email>
2025-06-23 21:10:47 -0700

    Day 14 done

--- 2024/Day_09/Day_09.lisp
+++ 2024/Day_09/Day_09.lisp
@@ -1,8 +1,8 @@
-;;;; Day###.lisp
-;;;; 2024 AOC Day ### solution
+;;;; Day09.lisp
+;;;; 2024 AOC Day 09 solution
 ;;;; Common Lisp solutions by Leo Laporte (with lots of help)
-;;;; Started:
-;;;; Finished: 
+;;;; Started: 2025-06-21
+;;;; Finished:
 
 ;; ----------------------------------------------------------------------------
 ;; Prologue code for setup - same every day
@@ -11,7 +11,7 @@
 (ql:quickload '(:fiveam :iterate :cl-ppcre :trivia :serapeum :str))
 (use-package :iterate) ; use iter instead of LOOP
 
-(defpackage :day###
+(defpackage :day09
   (:use  #:cl :iterate)
   (:local-nicknames
    (:re :cl-ppcre)       ; regex
@@ -19,19 +19,36 @@
    (:tr :trivia)         ; pattern matching
    (:5a :fiveam)))       ; testing framework
 
-(in-package :day###)
+(in-package :day09)
 
 (setf 5a:*run-test-when-defined* t)  ; test as we go
 (declaim (optimize (debug 3)))       ; max debugging info
 ;; (declaim (optimize (speed 3))     ; max speed if needed
 
-(defparameter *data-file* "~/cl/AOC/2024/Day_###/input.txt"
+(defparameter *data-file* "/Volumes/Data/Source/lisp/AOC/2024/Day_09/input.txt"
   "Downloaded from the AoC problem set")
 
 #| ----------------------------------------------------------------------------
+--- Day 9: Disk Fragmenter ---
 --- Part One ---
 
+"
+
+"Compact the amphipod's hard drive using the process he requested. What is the resulting filesystem checksum?"
+
+LEO'S NOTES:
+
+So the data set is a 20000 character string! I have to read it into
+memory because I'm moving files from the end, no sequential reads
+here.
+
+What's a good way of representing this data? I don't think I want to
+expand it out. It will be gigantic.
+
+
 ---------------------------------------------------------------------------- |#
+
+(defparameter *example* "2333133121414131402")
 
 
 
@@ -41,11 +58,11 @@
 ---------------------------------------------------------------------------- |#
 
 ;; now solve the puzzle!
-;; (time (format t "The answer to AOC 2024 Day ### Part 1 is ~a"
-;;	      (day###-1 (uiop:read-file-lines *data-file*))))
+;; (time (format t "The answer to AOC 2024 Day 09 Part 1 is ~a"
+;;	      (day09-1 (uiop:read-file-lines *data-file*))))
 
-;; (time (format t "The answer to AOC 2024 Day ### Part 2 is ~a"
-;;	      (day###-2 (uiop:read-file-lines *data-file*))))
+;; (time (format t "The answer to AOC 2024 Day 09 Part 2 is ~a"
+;;	      (day09-2 (uiop:read-file-lines *data-file*))))
 
 ;; ----------------------------------------------------------------------------
 ;; Timings with SBCL on an M4 Pro Mac mini with 64GB RAM
