fcc3ffc629a6835be3345037a2ee5c6958f48aaa
Leo Laporte <leo@laporte.email>
2025-12-01 23:09:27 -0800

    Day 2 done!

--- 2025/Day02/Day02.lisp
+++ 2025/Day02/Day02.lisp
@@ -34,12 +34,21 @@
 ;; LEO'S NOTES: We're given a string with a list of number
 ;; ranges. We're to count the invalid ids in those ranges. An ID is
 ;; invalid if it is "made only of some sequence of digits repeated
-;; twice." Sounds like a job for regex. A doubled sequence would look
-;; like this: ^(\\d+)\\1$
+;; twice." Sounds like a job for regex.
+;;
+;; Regex for a doubled sequence would look like this: ^(\\d+)\\1$
+;;
+;; (\\d+) is a group that contains one or more digits, and the \\1
+;; means repeat that group, i.e. a doubled sequence. The ^ and $ are
+;; anchors for the beginning and end of the string, thus we get a
+;; doubled sequence and ONLY a doubled sequence, no extraneous
+;; characters. (Sorry about the \\ - Common Lisp requires the \ to be
+;; escaped... with \. In Perl or Python it would just be ^(\d+)\1$).
+;;
 ;; ----------------------------------------------------------------------------
 
-(Defparameter *example* "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659, 824824821-824824827,2121212118-2121212124")
+;; (Defparameter *example* "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+;; 1698522-1698528,446443-446449,38593856-38593862,565653-565659, 824824821-824824827,2121212118-2121212124")
 
 (defparameter *twice-repeated-sequence* (re:create-scanner "^(\\d+)\\1$")
   "a regex for finding numbers consisting only of a twice repeating digit
@@ -57,9 +66,9 @@ sequence e.g. 11 234234 56785678")
 
 (sr:-> valid-id? (number function) boolean)
 (defun valid-id? (n regex)
-  "returns t if an id is valid (has no repeating digit sequences using
-the specified regex)"
-  (zerop (re:count-matches regex (write-to-string n))))
+  "returns t if an id is valid i.e. does NOT contain the regex for an
+invalid ID"
+  (not (re:scan regex (write-to-string n))))
 
 (5a:test invalid-id?-test
   (5a:is-true (valid-id? 123456 *twice-repeated-sequence*))
@@ -70,8 +79,8 @@ the specified regex)"
 
 (sr:-> sum-invalid-ids (number number function) number)
 (defun sum-invalid-ids (start end regex)
-  "given a range from start to end inclusive, count the number of invalid
-IDs in the range"
+  "given a range from start to end inclusive, sum the invalid
+IDs within the range"
   (iter (for n from start to end)
     (summing
       (if (not (valid-id? n regex)) n 0))))
@@ -97,18 +106,20 @@ IDs in the range"
 ;; ----------------------------------------------------------------------------
 ;;                              --- Part Two --
 ;;
-;; LEO'S NOTES: Just change the regex to "^(\\d+)\\1+$ (adding the
-;; plus after the 1) - I'll refactor part one to allow different
-;; regexes and we're done.
+;; LEO'S NOTES: This one's easy. Just change the regex to
+;; "^(\\d+)\\1+$ (adding the plus after the 1 to mean the group can be
+;; repeated one or more times) - I'll refactor part one to allow
+;; plug-in regexes and we're done. (cl-ppcre's CREATE-SCANNER compiles
+;; the regex string into a function which makes it somewhat faster).
 ;;
 ;; ----------------------------------------------------------------------------
 
-;; The modified regex (I use create-scanner to optimize the regex scan)
+;; The modified regex
 (defparameter *multiple-repeated-sequence* (re:create-scanner "^(\\d+)\\1+$")
   "a regex for finding numbers consisting only of a repeating
 digit sequence e.g. 1111 234234234234 56785678567856785678")
 
-(5a:test day02-1-test
+(5a:test day02-1-test                   ; I can reuse part 1 entirely
   (5a:is (= 4174379265 (day02-1 *example* *multiple-repeated-sequence*))))
 
 ;; ----------------------------------------------------------------------------
@@ -128,18 +139,18 @@ digit sequence e.g. 1111 234234234234 56785678567856785678")
 
 ;; The answer to AOC 2025 Day 2 Part 1 is 23560874270
 ;; Evaluation took:
-;; 0.393 seconds of real time
-;; 0.394625 seconds of total run time (0.392416 user, 0.002209 system)
-;; [ Real times consist of 0.007 seconds GC time, and 0.386 seconds non-GC time. ]
-;; [ Run times consist of 0.007 seconds GC time, and 0.388 seconds non-GC time. ]
-;; 100.51% CPU
-;; 286,780,400 bytes consed
+;; 0.307 seconds of real time
+;; 0.309322 seconds of total run time (0.306807 user, 0.002515 system)
+;; [ Real times consist of 0.007 seconds GC time, and 0.300 seconds non-GC time. ]
+;; [ Run times consist of 0.007 seconds GC time, and 0.303 seconds non-GC time. ]
+;; 100.65% CPU
+;; 286,715,056 bytes consed
 
 ;; The answer to AOC 2025 Day 2 Part 2 is 44143124633
 ;; Evaluation took:
-;; 0.404 seconds of real time
-;; 0.406438 seconds of total run time (0.403048 user, 0.003390 system)
-;; [ Real times consist of 0.009 seconds GC time, and 0.395 seconds non-GC time. ]
-;; [ Run times consist of 0.009 seconds GC time, and 0.398 seconds non-GC time. ]
-;; 100.50% CPU
-;; 350,507,072 bytes consed
+;; 0.325 seconds of real time
+;; 0.326735 seconds of total run time (0.324710 user, 0.002025 system)
+;; [ Real times consist of 0.007 seconds GC time, and 0.318 seconds non-GC time. ]
+;; [ Run times consist of 0.007 seconds GC time, and 0.320 seconds non-GC time. ]
+;; 100.62% CPU
+;; 350,455,200 bytes consed
